4d2de00a3be18d7a444ac45f73588e99f0348485
imran <imrankabir1996@gmail.com>
2021-07-02 22:04:32 +0600

    updated new page

--- marquee_hvac/home/static/home/js/apps.js
+++ marquee_hvac/home/static/home/js/apps.js
@@ -1,18 +1,26 @@
-//Navigation
- const navOpen = document.querySelector(".nav__hamburger");
- const navClose  = document.querySelector(".close__toggle");
- const menu = document.querySelector(".nav__menu");
+// hamburger manu
+const navOpen = document.querySelector(".nav__hamburger");
+const navClose = document.querySelector(".close__toggle");
+const menu = document.querySelector(".nav__menu");
+const scrollLink = document.querySelectorAll(".scroll-link");
+const navContainer = document.querySelector(".nav__menu");
 
- navOpen.addEventListener('click', ()=>{
-     menu.classList.add('open');
-     document.body.classList.add('active');
- });
- navClose.addEventListener('click', ()=>{
-     menu.classList.remove('open');
-     document.body.classList.remove('active');
- });
+// login
 
 
+// hamburger manu
+navOpen.addEventListener("click", () => {
+  menu.classList.add("open");
+  document.body.classList.add("active");
+  navContainer.style.left = "0";
+  navContainer.style.width = "30rem";
+});
 
-//AOS
-AOS.init();
+navClose.addEventListener("click", () => {
+  menu.classList.remove("open");
+  document.body.classList.remove("active");
+  navContainer.style.left = "-30rem";
+  navContainer.style.width = "0";
+});
+
+// login
